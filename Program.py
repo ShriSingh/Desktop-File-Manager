7dcaa8b510a011da01cd1f4714174f37920dd0be
Shriyansh Singh <shriyansh.singh01@student.csulb.edu>
2023-12-29 22:23:08 -0800

    Creating a file, mentioning the functions to be created

---
+++ Program.py
@@ -0,0 +1,30 @@
+# Importing libraries to manipulate files and folders
+import os
+import shutil
+
+def removing_files():
+    """
+    This function removes files from a folder after a specified time
+    """
+    #TODO: Removing files from folder
+    pass
+
+def organizing_files():
+    """
+    This function organizes files from a folder based on their extensions
+    or file type
+    """
+    #TODO: Organizing files from folder
+    pass
+
+def copying_files():
+    """
+    This function copies files from a folder to another folder
+    """
+    #TODO: Copying files from folder
+    pass
+
+if __name__ == "__main__":
+    removing_files()
+    organizing_files()
+    copying_files()

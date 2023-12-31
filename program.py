4505ee8568033b82fde04ebe5c983a6fbdf92d90
Shriyansh Singh <shriyansh.singh01@student.csulb.edu>
2023-12-30 20:06:02 -0800

    Working on changing the directory path absed on input

--- program.py
+++ program.py
@@ -1,9 +1,54 @@
 """For ending the program"""
 import sys
-"""Importing libraries to navigate and manipulate files and folders"""
+# Importing libraries to navigate and manipulate files and folders
 import os
 import shutil
 
+
+# Aiming for the function to be able to navigate to the following directories:
+# C:\Users\User\OneDrive\Pictures\Screenshots
+# C:\Users\User\Downloads
+def navigate_directory(input):
+    """
+    This function navigates and changes to the directory entered by the user
+    : param input: Directory chosen by the user
+    : return: Newly set path of the directory
+    """ 
+    # Setting the target directory
+    target_directory = input
+    # Finding the current working directory
+    current_path = os.getcwd()
+    
+    # Changing directories based on the target_directory
+    if target_directory == "Screenshots":
+        current_path = os.chdir("C:\\Users\\User\\OneDrive\\Pictures\\Screenshots")
+    elif target_directory == "Downloads":
+        current_path = os.chdir("C:\\Users\\User\\Downloads")
+    
+    return current_path
+
+
+def organizing_files():
+    """
+    This function organizes files from a folder based on their extensions
+    or file type
+    """
+    #TODO: Organizing files from folder
+    
+
+def removing_files():
+    """
+    This function removes files from a folder after a specified time
+    """
+    #TODO: Removing files from folder
+    pass
+
+
+def copying_files():
+    """
+    This function copies files from a folder to another folder
+    """
+    #TODO: Copying files from folder
 
 def menu():
     """
@@ -38,46 +83,11 @@ def menu():
         print("Exiting the program...")
         sys.exit()
     else:
+        # If the user enters an invalid choice, restart the menu
         print("You did not enter a valid choice. Please try again.\n")
         menu()
 
-
-# Aiming for the function to be able to navigate to the following directories:
-# C:\Users\User\OneDrive\Pictures\Screenshots
-# C:\Users\User\Downloads
-def navigate_directory(input):
-    """
-    This function navigates to a directory entered by the user
-    : param input: Directory chosen by the user
-    """ 
-    # Setting the target directory
-    target_directory = input
-    pass
-
-
-def removing_files():
-    """
-    This function removes files from a folder after a specified time
-    """
-    #TODO: Removing files from folder
-    pass
-
-
-def organizing_files():
-    """
-    This function organizes files from a folder based on their extensions
-    or file type
-    """
-    #TODO: Organizing files from folder
-
-
-def copying_files():
-    """
-    This function copies files from a folder to another folder
-    """
-    #TODO: Copying files from folder
-
-
 if __name__ == "__main__":
     # Welcome message, the menu screen and the user's choice
-    menu()
+    # menu()
+    navigate_directory("Screenshots")

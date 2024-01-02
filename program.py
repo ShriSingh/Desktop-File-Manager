aeb19abca8f887d1f975d34ccab8bc159b97482d
Shriyansh Singh <shriyansh.singh01@student.csulb.edu>
2024-01-01 21:44:44 -0800

    Editing imported modules and moving menu operations to menu file 1

--- program.py
+++ program.py
@@ -1,7 +1,7 @@
-"""Imported modules/packages"""
-import sys
-# Libraries to navigate and manipulate files and folders
+"""Imported modules"""
+# Module to navigate and manipulate files and folders
 import os
+# Module to conduct high-level file operations
 import shutil
 
 
@@ -24,17 +24,19 @@ def navigate_directory(target_directory):
     return current_path
 
 
-def organizing_files():
+def organizing_files(filter_type):
     """
     This function organizes files from a folder based on either their extension
     or year of creation
     """
     # TODO - Organize files based on their extensions
-    # Asking user which folder they want to organize
-    folder_name = input("Choose the folder you want to organize: ")
-    # Finding the current working directory path
-    dir_path = navigate_directory(folder_name)
-    print("The current working directory is: ", dir_path)
+    # Acting based on type of organization the user wants
+    if filter_type == 1:
+        pass
+    elif filter_type == 2:
+        pass
+
+    pass
 
 
 def removing_files():
@@ -46,7 +48,6 @@ def removing_files():
     folder_name = input("Choose the folder you want to remove files from: ")
     # Finding the current working directory path
     dir_path = navigate_directory(folder_name)
-    print("The current working directory is: ", dir_path)
 
 
 def copying_files():
@@ -62,47 +63,3 @@ def copying_files():
     folder_name = input("Choose the folder you want to copy files to: ")
     # Finding the current working directory path
     dir_path = navigate_directory(folder_name)
-
-
-def menu():
-    """
-    This function displays a welcome message to the user
-    """
-    # Welcome message
-    print("***************Welcome to Desktop File Manager!****************")
-    print("This program will help you organize your files on your desktop.")
-    print("***************************************************************\n")
-
-    # Showing the Menu Screen
-    print("***************Menu****************")
-    print("1. Organize files")
-    print("2. Copy files")
-    print("3. Remove files")
-    print("4. Exit")
-    print("***********************************\n")
-
-    # Asking the user for their choice
-    choice = input("Please enter your choice from the above menu: ")
-    # Action based on the user's choice
-    if choice == "1":
-        print("Organizing the files in the folder...")
-        organizing_files()
-    elif choice == "2":
-        print("Copying files from one folder to another...")
-        copying_files()
-    elif choice == "3":
-        print("Removing the files from the folder...")
-        removing_files()
-    elif choice == "4":
-        print("Exiting the program...")
-        sys.exit()
-    else:
-        # If the user enters an invalid choice, restart the menu
-        print("You did not enter a valid choice. Please try again.\n")
-        menu()
-
-
-if __name__ == "__main__":
-    # Welcome message, the menu screen and the user's choice
-    # menu()
-    navigate_directory("OneDrive - CSULB")
